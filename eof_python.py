04687fd3af6293b63add0331551022609aefdc37
Mindy McAdams <mmcadams@gmail.com>
2015-02-08 10:49:02 -0500

    add EOF example

---
+++ eof_python.py
@@ -0,0 +1,9 @@
+with open('somefile') as openfileobject:
+    for line in openfileobject:
+        do_something()
+
+"""
+http://stackoverflow.com/questions/15599639/whats-perfect-counterpart-in-python-for-while-not-eof 
+"""
+
+
